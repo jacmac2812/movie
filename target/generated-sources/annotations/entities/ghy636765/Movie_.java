3fee262fc51790392772b0a105a2742177b88d33
jacmac2812 <simonsen.jac@icloud.com>
2020-09-12 13:42:45 +0200

    first commit

---
+++ target/generated-sources/annotations/entities/ghy636765/Movie_.java
@@ -0,0 +1,19 @@
+package entities.ghy636765;
+
+import entities.Movie;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-12T13:22:53")
+@StaticMetamodel(Movie.class)
+public class Movie_ { 
+
+    public static volatile SingularAttribute<Movie, Integer> year;
+    public static volatile SingularAttribute<Movie, String> director;
+    public static volatile SingularAttribute<Movie, String> mainMaleActor;
+    public static volatile SingularAttribute<Movie, Long> id;
+    public static volatile SingularAttribute<Movie, String> title;
+    public static volatile SingularAttribute<Movie, String> mainFemaleActor;
+
+}
